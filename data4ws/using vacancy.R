fa53eb6e7659573b671c4c05027021569bab2091
Xavier Timbeau <54633745+xtimbeau@users.noreply.github.com>
2025-08-26 18:28:19 +0200

    vacance par iris

--- data4ws/using vacancy.R
+++ data4ws/using vacancy.R
@@ -6,3 +6,19 @@ vacancy <- open_dataset("data4ws/c200_amp.parquet") |>
 
 vacancy |> write_parquet("data4ws/vacancy2.parquet")
 vacancy |> st_transform(3857) |> st_write("data4ws/vacancy2.geojson")
+
+
+iris <- open_dataset("data4ws/iris_amp.parquet") |> st_as_sf()
+
+vacancy.iris <- open_dataset("data4ws/vacancy2.parquet") |> 
+  st_as_sf() |> 
+  group_by(IRIS) |> 
+  st_drop_geometry() |> 
+  summarize(
+    across(-c(idINS, INSEE_COM), ~sum(.x, na.rm=TRUE) ) ) |> 
+  left_join(iris, by = "IRIS")
+
+vacancy.iris |> write_parquet("data4ws/vacancy_iris.parquet")
+
+vacancy.iris |> st_transform(3857) |> write_parquet("data4ws/vacancy_iris.geojson")
+    
